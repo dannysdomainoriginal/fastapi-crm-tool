caa5ed2382d38f414b1fd209a0da17b671f7340c
Charles Daniel <dannysdomainoriginal@gmail.com>
2026-02-17 00:01:02 +0100

    chore: fixed axiosInstance baseURL

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -11,7 +11,7 @@ import type {
   TaskUpdate
 } from '../types';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = import.meta.env.DEV ? "http://localhost:8000" : undefined
 
 const api = axios.create({
   baseURL: API_URL,
